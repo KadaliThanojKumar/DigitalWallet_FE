9b06a82230165b7c713e56fb147670ab55021b3b
THANOJ KUMAR KADALI <162683181+KadaliThanojKumar@users.noreply.github.com>
2025-09-23 21:09:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "./", // Ensures correct relative paths
+//   root: "./",
+//   publicDir: "public", // Ensures `index.html` is correctly located
+// });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Ensures correct relative paths
-  root: "./",
-  publicDir: "public", // Ensures `index.html` is correctly located
+  base: '/',  // Root path for deployment
 });
+
