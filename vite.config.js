23061b7b282889e5cddfc335c4fd089a1946fb6b
THANOJ KUMAR KADALI <162683181+KadaliThanojKumar@users.noreply.github.com>
2025-09-25 12:33:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Ensures correct relative paths
-  root: "./",
-  publicDir: "public", // Ensures `index.html` is correctly located
-});
-
-
+  base: '/DigitalWallet_FE/'  // <-- your repo name
+})
