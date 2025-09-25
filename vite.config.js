c3117d82d564e8c2182cc1f853a7fa115cc70850
KadaliThanojKumar <2300032859@kluniversity.in>
2025-09-25 12:38:58 +0530

    rr

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/", // Ensures correct relative paths
+//   root: "./",
+//   publicDir: "public", // Ensures `index.html` is correctly located
+// });
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
