e8c7a40d3a6dfc5653e25ac07a8327cfc2af770c
KadaliThanojKumar <2300032859@kluniversity.in>
2025-09-27 09:50:06 +0530

    jj

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,29 @@
+// // src/index.js
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css'; // Global styles (Tailwind CSS)
+// import App from './App'; // Import App.jsx
+// import { BrowserRouter } from 'react-router-dom';
+// import { HashRouter as Router } from 'react-router-dom';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Global styles (Tailwind CSS)
 import App from './App'; // Import App.jsx
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
+
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Ensures correct relative paths
+  //base: "/", // Ensures correct relative paths
+  base: "/DigitalWallet_FE/",
   root: "./",
   publicDir: "public", // Ensures `index.html` is correctly located
 });
